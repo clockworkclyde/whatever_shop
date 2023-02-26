aa1025b6a6edc76ab43208dab228a94ae9e58a46
clockworkclyde <103137991+clockworkclyde@users.noreply.github.com>
2023-02-27 02:48:40 +0500

    include toolbar to shop categories ui

--- app/src/main/java/com/github/clockworkclyde/eshop/ui/categories/ShopCategoriesViewModel.kt
+++ app/src/main/java/com/github/clockworkclyde/eshop/ui/categories/ShopCategoriesViewModel.kt
@@ -2,20 +2,21 @@ package com.github.clockworkclyde.eshop.ui.categories
 
 import androidx.lifecycle.viewModelScope
 import com.github.clockworkclyde.core.common.FlowResultList
-import com.github.clockworkclyde.core.common.StateFlowList
 import com.github.clockworkclyde.core.navigation.INavigator
 import com.github.clockworkclyde.core.presentation.viewmodels.BaseFlowViewModel
 import com.github.clockworkclyde.core.presentation.viewmodels.INavigationViewModel
-import com.github.clockworkclyde.core.utils.*
+import com.github.clockworkclyde.core.utils.loadingResult
 import com.github.clockworkclyde.domain.model.product.BaseProductCard
 import com.github.clockworkclyde.domain.model.product.CommonCategory
-import com.github.clockworkclyde.domain.model.product.CommonCategoryHorizontalItem
 import com.github.clockworkclyde.domain.model.product.ProductCardHorizontalItem
 import com.github.clockworkclyde.domain.usecases.products.GetCommonCategoriesHorizontalItemUseCase
 import com.github.clockworkclyde.domain.usecases.products.GetProductCategoriesHorizontalUseCase
 import com.github.clockworkclyde.eshop.navigation.directions.categories.ShopCategoriesDirections
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.shareIn
 import javax.inject.Inject
 
 @HiltViewModel
